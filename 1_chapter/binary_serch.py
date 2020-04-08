91b0d85c8110fbf8cce244757810bfd619b87761
1_Hours <sharipov.yanepenb@gmail.com>
2020-04-08 14:14:37 +0700

    first commit

---
+++ 1_chapter/binary_serch.py
@@ -0,0 +1,24 @@
+from math import floor
+#Бинарный поиск
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def binary_serch(coll, num):
+  low = 0 #low и high - это границы списка
+  high = len(coll) -1
+
+  while low <= high:
+    mid = floor((low + high) / 2) # если значение не чётно, окр. в меньшую сторону
+    #print(mid)
+    guess = coll[mid]
+    if guess == num:
+      print(mid)
+      return mid # значение(index) найдено
+    elif guess > num:
+      high = mid - 1
+    else:
+      low = mid + 1
+    #print(guess)
+  return None
+
+binary_serch(numbers, 10)
+
